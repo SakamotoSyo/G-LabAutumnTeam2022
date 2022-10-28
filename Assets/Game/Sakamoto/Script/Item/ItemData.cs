6b1202057021eec489cdacf866c18deb9c6131b3
sakamotosyo <syo10150000@gmail.com>
2022-10-28 22:38:06 +0900

    [Fix]製造所試行錯誤中

--- Assets/Game/Sakamoto/Script/Item/ItemData.cs
+++ Assets/Game/Sakamoto/Script/Item/ItemData.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="SakamotoScriptable/ItemData")]
 public class ItemData : MonoBehaviour
 {
+    [Header("アイテム名")]
+    [SerializeField] public string ItemName;
+
     [Header("加工できるかどうか")]
     [SerializeField] public bool Processing;
 
     [Header("製造できるかどうか")]
     [SerializeField] public bool Craft;
+
+    [Header("梱包できるか")]
+    [SerializeField] public bool Packing;
 
     [Header("アイテムデータのプレハブ")]
     [SerializeField] public GameObject ItemPrefab;
