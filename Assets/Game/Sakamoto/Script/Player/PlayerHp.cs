be6cb124db16bdb662f324d35afc3b2b7c43792c
sakamotosyo <syo10150000@gmail.com>
2022-10-30 15:41:18 +0900

    [Add]PlayerのHp機能を追加

--- Assets/Game/Sakamoto/Script/Player/PlayerHp.cs
+++ Assets/Game/Sakamoto/Script/Player/PlayerHp.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 using System;
 public class PlayerHp : MonoBehaviour, IDamage
 {
-   [Header("プレイヤーのHp")]
-   [SerializeField] float _playerHp = 0;
-   
+    [Header("PlayerのMaxHp")]
+    [SerializeField] float _playerMaxHp;
+    [Tooltip("プレイヤーのHp")]
+    float _playerHp;
+    /// <summary>HPが０になった時通知するAction</summary>
+    public Action OnDead;
 
-   public Action OnDead;  
-    
+    /// <summary>
+    /// ダメージを受けるときに呼ばれる関数
+    /// </summary>
+    /// <param name="amount">ダメージ数</param>
     public void ApplyDamage(float amount)
     {
-        throw new System.NotImplementedException();
+        _playerHp -= amount;
+        //体力が０以下になった時通知する
+        if (_playerHp <= 0)
+        {
+            OnDead?.Invoke();
+        }
     }
 
+    /// <summary>
+    /// 回復するときに呼ぶ関数
+    /// </summary>
+    /// <param name="amount">回復する量</param>
     public void ApplyHeal(float amount)
     {
-        throw new System.NotImplementedException();
-    }
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
+        _playerHp += amount;
     }
 }
