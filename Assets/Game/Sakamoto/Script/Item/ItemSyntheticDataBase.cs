b0bc52396bd67f38e13d8693fef8834fca11363c
sakamotosyo <syo10150000@gmail.com>
2022-11-01 23:03:26 +0900

    [Add]playerのinventoryを作成

--- Assets/Game/Sakamoto/Script/Item/ItemSyntheticDataBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SakamotoScriptable/SyntheticDataBase")]
-public class ItemSyntheticDataBase : ScriptableObject
-{
-    public List<ItemSynthetic> SyntheticList = new List<ItemSynthetic>();
-}
