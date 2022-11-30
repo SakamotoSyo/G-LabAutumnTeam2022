7c5d48ac4e1912805c67f8d3bc868d21d9908356
Ryuto034 <ryukyu.river0309@gmail.com>
2022-11-30 18:09:36 +0900

    [Update]リザルトシーンの流れを変更 [Add]タイトル・リザルトのUI素材差し替え

--- Assets/Game/Kawabata/Script/Title/LevelSelect.cs
+++ Assets/Game/Kawabata/Script/Title/LevelSelect.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
-    [SerializeField] private GameObject _selected;
     //ボタンが押されたら難易度を変更するようなスクリプトにしたい
     public int _level;
+    [SerializeField]
+    public GameObject[] _selected;
 
     public void ChangeLevel(int level)
     {
-        _selected.GetComponent<RectTransform>().anchoredPosition = new Vector2(-175 + level * 175,0) ;
         _level = level;
+        for(var i = 0; i < _selected.Length; i++)
+        {
+            if(i == _level)
+            {
+                _selected[i].SetActive(true);
+            }
+            else
+            {
+                _selected[i].SetActive(false); 
+            }
+        }
     }
 
     public void GameStart()
