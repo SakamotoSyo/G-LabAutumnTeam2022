bc25e902ba2bf456a978b66016b3d75ccdcc7f8c
sakamotosyo <syo10150000@gmail.com>
2022-12-16 22:04:47 +0900

    [Fix]SnowFloorの修正

--- Assets/Game/Kojima/Scripts/Task/SnowFloor.cs
+++ Assets/Game/Kojima/Scripts/Task/SnowFloor.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SnowFloor : MonoBehaviour
 {
     #region 定義
-    GameObject _player1;
-    GameObject _player2;
+    [SerializeField] PlayerHp _player1;
+    [SerializeField] PlayerHp _player2;
     Vector2 _player1_curPos;//現在の座標
     Vector2 _player2_curPos;
     Vector2 _player1_prevPos;//過去の座標
@@ -19,8 +19,7 @@ public class SnowFloor : MonoBehaviour
 
     void Start()
     {
-        _player1 = GameObject.Find("Player1");
-        _player2 = GameObject.Find("Player2");
+     
     }
 
     
@@ -34,36 +33,38 @@ public class SnowFloor : MonoBehaviour
     //プレイヤーのHP回復を行う
     private void FixedUpdate()
     {
+      
         /*----プレイヤーの移動を管理しているクラスから移動した後の座標を取得----*/
-        _player1_curPos = new Vector2(_player1.transform.position.x,0);
-        _player2_curPos = new Vector2(_player2.transform.position.x,0);
-        //移動距離の計算
-        _player1_dis = _player1_prevPos + _player1_curPos;
-        _player2_dis = _player2_prevPos + _player2_curPos;
-        //移動距離に応じてHPを回復
-        playerCon.playerHp += hpBase * _player1_dis.magnitude;
-        playerCon.playerHp += hpBase * _player2_dis.magnitude;
-
-        _player1_prevPos = _player1_curPos;
-        _player2_prevPos= _player2_curPos;
+       
 
     }
     #endregion
 
 
     #region 当たり判定
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         //tagでプレイヤーとSnowFloorの接触を判定
         if (collision.gameObject.tag == "Player1")
         {
             //プレイヤーがSnowFloorに入った瞬間の座標を取得
             /*----プレイヤーの移動を管理しているクラスから取得----*/
-            _player1_prevPos = new Vector2(_player1.transform.position.x, 0);
+            _player1_curPos = new Vector2(_player1.transform.position.x, 0);
+            //移動距離の計算
+            _player1_dis = _player1_prevPos - _player1_curPos;
+            //移動距離に応じてHPを回復
+            _player1.ApplyHeal(hpBase * _player1_dis.magnitude);
+           
+
+            _player1_prevPos = _player1_curPos;
+           
         }
         if(collision.gameObject.tag == "Player2")
         {
-            _player2_prevPos = new Vector2(_player2.transform.position.x, 0);
+            _player2_curPos = new Vector2(_player2.transform.position.x, 0);
+            _player2_dis = _player2_prevPos + _player2_curPos;
+            _player2.ApplyHeal(hpBase * _player2_dis.magnitude);
+            _player2_prevPos = _player2_curPos;
         }
     }
     #endregion
